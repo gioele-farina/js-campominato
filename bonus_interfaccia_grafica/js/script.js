3f96ac454738b91c04b654d5ff62ff9cdb00c0f6
Gioele Farina <jchilosa@gmail.com>
2020-10-28 18:09:21 +0100

    campo con bombe e numeri fare test

--- bonus_interfaccia_grafica/js/script.js
+++ bonus_interfaccia_grafica/js/script.js
@@ -11,6 +11,7 @@ var numeroCasuale;
 var exit;
 var x;
 var y;
+var conteggioBombe;
 
 // Non utilizzo l'intera matrice ma i bordi esterni li lascio vuoti. C'è una specie di cornice vuota.
 var campoFiorito = [
@@ -52,13 +53,59 @@ for (var i = 0; i < numeroBombe; i++) {
 
 }
 
+console.log("Campo con bombe");
 console.log(campoFiorito);
 
+/*
+Assegno i numerini alle caselle in base alle bombe che ci sono vicino
+Per ogni "casella" da [1][1] a [10][10] controllo le caselle adiecenti e conto le bombe.
+*/
+  // per ogni casella
+x = 0;
+y = 0;
+for (var x = 1; x <= 10; x++) {
+  for (var y = 1; y <= 10; y++) {
+    // casella: campoFiorito[x][y]
+    // se nella casella non c'è una bomba, conta le bombe nelle caselle adiacenti
+    conteggioBombe = 0;
+    if (campoFiorito[x][y] != "B") {
 
+      if (campoFiorito[x][y-1] === "B") {
+        conteggioBombe++;
+      }
+      if (campoFiorito[x][y+1] === "B") {
+        conteggioBombe++;
+      }
+      if (campoFiorito[x-1][y-1] === "B") {
+        conteggioBombe++;
+      }
+      if (campoFiorito[x-1][y] === "B") {
+        conteggioBombe++;
+      }
+      if (campoFiorito[x-1][y+1] === "B") {
+        conteggioBombe++;
+      }
+      if (campoFiorito[x+1][y-1] === "B") {
+        conteggioBombe++;
+      }
+      if (campoFiorito[x+1][y] === "B") {
+        conteggioBombe++;
+      }
+      if (campoFiorito[x+1][y+1] === "B") {
+        conteggioBombe++;
+      }
 
+      // Assegno le bombe
+      campoFiorito[x][y] = conteggioBombe;
 
+    }
 
+  }
+}
 
+console.log("------------------------------------------------------------");
+console.log("Campo con bombe e numeri");
+console.log(campoFiorito);
 
 
 
